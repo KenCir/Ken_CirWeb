b4354edfd29d95f850d79af3d71f2114a2776929
Ken_Cir0909 <65713982+KenCir@users.noreply.github.com>
2022-07-26 20:21:36 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-app id="app">
+    <header>
+      <v-app-bar app color="primary" class="lighten-1">
+        <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+        <v-toolbar-title class="hidden-sm-and-down">Ken_Cir Website</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-container class="text-right">
+          <span class="mr-2">最終更新 2022年7月26日</span>
+        </v-container>
+      </v-app-bar>
+
+      <v-navigation-drawer v-model="drawer" fixed temporary>
+        <v-list nav dense>
+          <v-list-item-group>
+            <v-list-item>
+              <v-list-item-title><router-link to="/">Home</router-link></v-list-item-title>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-title><router-link to="/about">About</router-link></v-list-item-title>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-navigation-drawer>
+    </header>
+
+    <v-main>
+      <router-view />
+    </v-main>
+
+    <footer>
+      <v-footer padless class="mt-16">
+        <v-col class="text-center" cols="12">
+          {{ new Date().getFullYear() }} — <strong>Ken_Cir</strong>
+        </v-col>
+      </v-footer>
+    </footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+
+  data: () => ({
+    drawer: false,
+  }),
+};
+</script>
+
+<style>
+@font-face {
+  font-family: "Roboto-ThinItalic";
+  src: url(@/assets/fonts/Roboto/Roboto-ThinItalic.ttf);
+}
+
+ .v-application--wrap {
+    min-height: -webkit-fill-available;
+  }
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="about">
+    <v-container class="text-center">
+      <h1 class="pa-10">About Me</h1>
+      <h3 class="mt-5">Name: Ken_Cir(けんちる)</h3>
+      <h3 class="mt-16">
+        趣味: プログラミング・イラスト・東方・STG・音ゲー・Vtuberなど
+      </h3>
+      <h4 class="mt-16">プログラミングを趣味としている謎の人です<br />色んなものを気まぐれで作っています</h4>
+      <v-row class="mt-5">
+      <v-col>
+        <v-btn 
+          text
+          href="https://github.com/KenCir"
+          target="_blank"
+        >
+          <v-icon left color="#211F1F">mdi-github</v-icon>
+          <span>Github</span>
+        </v-btn>
+      </v-col>
+      <v-col>
+        <v-btn 
+          text
+          href="https://Twitter.com/Ken_Cir"
+          target="_blank"
+        >
+          <v-icon left color="#1DA1F2">mdi-twitter</v-icon>
+          <span>Twitter</span>
+        </v-btn>
+      </v-col>
+    </v-row>
+    </v-container>
+  </div>
+</template>
