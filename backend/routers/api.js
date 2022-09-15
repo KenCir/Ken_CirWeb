2d12a5f3e103b8e3525c7184eb6c035a070c84ba
Ken_Cir0909 <65713982+KenCir@users.noreply.github.com>
2022-09-15 16:50:25 +0900

    動的ブログ消すならbackendいらんやん

--- backend/routers/api.js
+++
@@ -1,6 +0,0 @@
-const { getAllBlog } = require('../database/database');
-const express = require('express');
-const router = express.Router();
-
-router.use('/blog', require('./blog'));
-module.exports = router;
